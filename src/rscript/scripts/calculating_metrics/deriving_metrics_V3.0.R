bba76ef4b8a644037c56f352c3f607ba3161e9d5
Jason <jasxavier7@gmail.com>
2024-09-10 15:08:38 +1200

    created a function for deriving metrics for multiple files

---
+++ src/rscript/scripts/calculating_metrics/deriving_metrics_V3.0.R
@@ -0,0 +1,18 @@
+# An attempt to derive all the metrics specified in the email
+# Everything was extracted to a separate file and refined
+# So this file is just testing that it works
+# TODO: maybe comment all of this a bit better
+
+source("scripts/calculating_metrics/metric_functions_v2.0.R")
+
+# Read processed data
+df <- readRDS("data/processed/empty_generated_0_shift_28.rds")
+final_metrics <- calculate_metrics(df)
+print(final_metrics)
+
+# Read processed data
+df <- readRDS("data/processed/empty_generated_0_shift_28.rds")
+df_1 <- readRDS("data/processed/empty_generated_1_shift_28.rds")
+df_2 <- readRDS("data/processed/empty_generated_2_shift_28.rds")
+
+final_metrics <- calculate_metrics_multiple(df, df_1, df_2)
---
+++ src/rscript/scripts/calculating_metrics/metric_functions_v2.0.R
@@ -0,0 +1,122 @@
+# Contains all the functions used to derive metrics
+# Do source("scripts/calculating_metrics/metric_functions.R") to use
+
+# average height ----------------------------------------------------------
+calculate_average_height <- function(df) {
+  df %>%
+    group_by(Coord_Index) %>%
+    summarise(
+      average_height = mean(Y, na.rm = TRUE),
+      sd_height = sd(Y, na.rm = TRUE)
+    )
+}
+
+# average velocity --------------------------------------------------------
+calculate_average_velocity <- function(df) {
+  df <- df %>%
+    group_by(Coord_Index) %>%
+    mutate(
+      dX = X - lag(X),
+      dY = Y - lag(Y),
+      dZ = Z - lag(Z),
+      time_diff = Iteration - lag(Iteration),
+      velocity_X = dX / time_diff,
+      velocity_Y = dY / time_diff,
+      velocity_Z = dZ / time_diff,
+      velocity_magnitude = sqrt(velocity_X^2 + velocity_Y^2 + velocity_Z^2)
+    ) %>%
+    ungroup() %>%
+    filter(!is.na(velocity_magnitude))
+  
+  df %>%
+    group_by(Coord_Index) %>%
+    summarise(
+      avg_velocity_magnitude = mean(velocity_magnitude, na.rm = TRUE),
+      sd_velocity_magnitude = sd(velocity_magnitude, na.rm = TRUE)
+    )
+}
+
+# average acceleration ----------------------------------------------------
+calculate_average_acceleration <- function(df) {
+  df <- df %>%
+    group_by(Coord_Index) %>%
+    mutate(
+      dX = X - lag(X),
+      dY = Y - lag(Y),
+      dZ = Z - lag(Z),
+      time_diff = Iteration - lag(Iteration),
+      velocity_X = dX / time_diff,
+      velocity_Y = dY / time_diff,
+      velocity_Z = dZ / time_diff,
+      velocity_magnitude = sqrt(velocity_X^2 + velocity_Y^2 + velocity_Z^2)
+    ) %>%
+    ungroup() %>%
+    filter(!is.na(velocity_magnitude)) %>%
+    group_by(Coord_Index) %>%
+    mutate(
+      dVx = velocity_X - lag(velocity_X),
+      dVy = velocity_Y - lag(velocity_Y),
+      dVz = velocity_Z - lag(velocity_Z),
+      time_diff_accel = Iteration - lag(Iteration),
+      acceleration_X = dVx / time_diff_accel,
+      acceleration_Y = dVy / time_diff_accel,
+      acceleration_Z = dVz / time_diff_accel,
+      acceleration_magnitude = sqrt(acceleration_X^2 + acceleration_Y^2 + acceleration_Z^2)
+    ) %>%
+    ungroup() %>%
+    filter(!is.na(acceleration_magnitude))
+  
+  df %>%
+    group_by(Coord_Index) %>%
+    summarise(
+      avg_acceleration_magnitude = mean(acceleration_magnitude, na.rm = TRUE),
+      sd_acceleration_magnitude = sd(acceleration_magnitude, na.rm = TRUE)
+    )
+}
+
+# combining everything ------------------------------------------------------
+calculate_metrics <- function(df) {
+  average_height <- calculate_average_height(df)
+  average_velocity <- calculate_average_velocity(df)
+  average_acceleration <- calculate_average_acceleration(df)
+  
+  final_metrics <- average_height %>%
+    left_join(average_velocity, by = "Coord_Index") %>%
+    left_join(average_acceleration, by = "Coord_Index")
+  
+  return(final_metrics)
+}
+
+# Define the function to process multiple data frames and combine metrics
+calculate_metrics_multiple <- function(...) {
+  # Capture the variable arguments as a list of data frames
+  data_frames <- list(...)
+  
+  # Initialize an empty list to store the results for each data frame
+  combined_metrics <- list()
+  
+  # Loop through each data frame and calculate the metrics
+  for (i in seq_along(data_frames)) {
+    # Access the current data frame
+    df <- data_frames[[i]]
+    
+    # Calculate metrics for the current data frame
+    file_metrics <- calculate_metrics(df)
+    
+    # Add a column for the file number (starting at 1)
+    file_metrics <- file_metrics %>%
+      mutate(file_number = i)
+    
+    # Reorder columns to make file_number the first column
+    file_metrics <- file_metrics %>%
+      select(file_number, everything())
+    
+    # Append the metrics to the list
+    combined_metrics[[i]] <- file_metrics
+  }
+  
+  # Combine all the results into one dataframe
+  final_combined_metrics <- bind_rows(combined_metrics)
+  
+  return(final_combined_metrics)
+}
