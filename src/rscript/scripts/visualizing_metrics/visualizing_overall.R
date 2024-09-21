4a830f041ccb3dba1ed405941dce27121953cc58
ahua352 <ahua352@aucklanduni.ac.nz>
2024-09-21 19:55:00 +1200

    feat: combined all plots into single image

--- src/rscript/scripts/visualizing_metrics/visualizing_overall.R
+++ src/rscript/scripts/visualizing_metrics/visualizing_overall.R
@@ -42,12 +42,25 @@ generate_boxplot <- function(body_part, metric) {
 body_parts <- c("ankle_left", "ankle_right", "pelvis", "solar_plexus", "wrist_left", "wrist_right")
 metrics <- c("height", "distance_moved", "acceleration_magnitude", "velocity_magnitude")
 
+plot_list <- list()
+
 # Generate and display plots for each combination
 for (body_part in body_parts) {
   for (metric in metrics) {
     plot <- generate_boxplot(body_part, metric)
     if (!is.null(plot)) {
-      print(plot)
+      # print(plot)
+      
+      # Individual images
+      # filename <- paste0("output/overall/boxplot_", body_part, "_", metric, ".png")
+      # ggsave(filename = filename, plot = plot, width = 8, height = 6, dpi = 300)
+      plot_list[[paste(body_part, metric, sep = "_")]] <- plot  # Store plot in list
+      
     }
   }
 }
+
+# Combine all plots into a single image
+combined_plot <- wrap_plots(plotlist = plot_list, ncol = 5)  # Adjust ncol for layout
+print(combined_plot)
+ggsave("output/overall/combined_boxplots.png", plot = combined_plot, width = 25, height = 20, dpi = 300) # Adjust width and height
