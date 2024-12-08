9c4017dbfe06e6b15183538389df267e21a5bafd
J.Z. Zhou <flamemaikuai@gmail.com>
2024-12-09 09:54:37 +1300

    new configs

---
+++ src/bailandopp/app/config/gpt_config_lb.py
@@ -0,0 +1,37 @@
+block_size = 29
+base = {
+    "embd_pdrop": 0.1,
+    "resid_pdrop": 0.1,
+    "attn_pdrop": 0.1,
+    "vocab_size_up": 2048,
+    "vocab_size_down": 2048,
+    "block_size": 29,
+    "n_layer": 6,
+    "n_head": 12,
+    "n_embd": 768 ,
+    "n_music": 768,
+    "n_music_emb": 768,
+    }
+head = {
+    "embd_pdrop": 0.1,
+    "resid_pdrop": 0.1,
+    "attn_pdrop": 0.1,
+    "vocab_size": 2048,
+    "block_size": 29,
+    "n_layer": 6,
+    "n_head": 12,
+    "n_embd": 768,
+    "vocab_size_up": 2048,
+    "vocab_size_down": 2048,
+    }
+critic_net = {
+    "embd_pdrop": 0.,
+    "resid_pdrop": 0.,
+    "attn_pdrop": 0.,
+    "block_size": 29,
+    "n_layer": 3,
+    "n_head": 12,
+    "n_embd": 768,
+    "vocab_size_up": 1,
+    "vocab_size_down": 1,
+    }
